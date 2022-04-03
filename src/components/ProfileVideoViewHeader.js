89604f1b4f793fdc8ad953fdc4adcf21bb7583af
hafizbali315 <hafizbali315@gmail.com>
2022-04-04 00:31:35 +0500

    Profile Page

---
+++ src/components/ProfileVideoViewHeader.js
@@ -0,0 +1,211 @@
+import useDarkMode from './../useDarkMode'
+
+import FavIconWhite from '../assets/images/fav-icon-white.png'
+import LogoSmDark from '../assets/images/logo-sm-dark.png'
+import LogoSmLight from '../assets/images/logo-sm-light.png'
+import UserAvatar from '../assets/images/user-avatar.png'
+
+const ProfileVideoViewHeader = () => {
+	const [colorTheme, setTheme] = useDarkMode()
+
+	return (
+		<>
+			{/* Desktop Header */}
+			<header className="desktop-header light-header d-lg-block d-none">
+				<nav className="navbar navbar-expand-lg navbar-light">
+					<div className="container-fluid">
+						<a href="home" className="navbar-brand">
+							<img src={FavIconWhite} alt="logo" className="img-fluid" />
+						</a>
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className="collapse navbar-collapse" id="navbarSupportedContent">
+							<div className="input-group input-group-lg">
+								<span className="input-group-text">
+									<i className="far fa-search"></i>
+								</span>
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Recipient's username"
+									aria-label="Recipient's username with two button addons"
+								/>
+								<button className="btn btn-orange z-index-1001" type="button">
+									Search
+								</button>
+								<button className="btn btn-blue z-index-1002" type="button">
+									Expand
+								</button>
+								<button className="btn btn-purple z-index-1003" type="button">
+									Reset
+								</button>
+							</div>
+							<ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+								<li className="nav-item">
+									<a className="nav-link ps-3 pe-0" href="#a">
+										<svg xmlns="http://www.w3.org/2000/svg" width="30" viewBox="0 0 49.721 46.613">
+											<path
+												id="Icon_metro-upload"
+												data-name="Icon metro-upload"
+												d="M24.324,28.717h6.215V16.286h9.323L27.431,3.856,15,16.286h9.323Zm9.323-6.992v4.793l14.23,5.307L27.431,39.448,6.986,31.824l14.23-5.307V21.725L2.571,28.717v12.43l24.86,9.323,24.86-9.323V28.717Z"
+												transform="translate(-2.571 -3.856)"
+												fill="#DBA5FA"
+											/>
+										</svg>
+									</a>
+								</li>
+								<li className="nav-item border-end">
+									<div className="dark-mode-toggle">
+										<div className="form-check form-switch">
+											<input className="form-check-input" type="checkbox" role="switch" onClick={() => setTheme(colorTheme)} />
+										</div>
+									</div>
+									{/* <a className="nav-link" className="dark-mode-toggle"><i className="fas fa-toggle-on"></i></a>  */}
+								</li>
+								<li className="nav-item dropdown">
+									<a
+										className="nav-link dropdown-toggle"
+										href="#a"
+										id="navbarDropdown"
+										role="button"
+										data-bs-toggle="dropdown"
+										aria-expanded="false"
+									>
+										<div className="d-flex align-items-center">
+											<div className="user-avatar flex-shrink-0">
+												<img src={UserAvatar} alt="avatar" className="img-fluid" />
+											</div>
+											<div className="text flex-grow-1">
+												Hello, <strong>David</strong>
+												<i className="fas fa-angle-down ps-2"></i>
+											</div>
+										</div>
+									</a>
+									<ul className="dropdown-menu dropdown-menu-end">
+										<li>
+											<a className="dropdown-item" href="#a">
+												Action
+											</a>
+										</li>
+										<li>
+											<a className="dropdown-item" href="#b">
+												Another action
+											</a>
+										</li>
+										<li>
+											<hr className="dropdown-divider" />
+										</li>
+										<li>
+											<a className="dropdown-item" href="#c">
+												Something else here
+											</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</header>
+
+			{/* Mobile Header  */}
+			<header className="mobile-header d-lg-none d-block shadow-sm border-bottom">
+				<div className="position-relative">
+					<nav className="navbar">
+						<div className="container d-flex">
+							<button className="btn side-nave-show-btn">
+								<i className="fas fa-bars"></i>
+							</button>
+							<div>
+								<a href="index.html" className="light-mode-logo navbar-brand">
+									<img src={LogoSmDark} alt="logo" className="img-fluid" />
+								</a>
+								<a href="index.html" className="dark-mode-logo navbar-brand">
+									<img src={LogoSmLight} alt="logo" className="img-fluid" />
+								</a>
+							</div>
+							<ul className="ps-0 d-flex align-items-center">
+								<li>
+									<button className="btn search-showHide-btn pt-2 px-0 collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+										<i className="far fa-search"></i>
+									</button>
+								</li>
+								<li>
+									<div className="dark-mode-toggle">
+										<div className="form-check form-switch">
+											<input className="form-check-input" type="checkbox" role="switch" onClick={() => setTheme(colorTheme)} />
+										</div>
+									</div>
+								</li>
+								<li>
+									<div className="dropdown">
+										<a
+											className="dropdown-toggle"
+											href="#a"
+											id="navbarDropdown"
+											role="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false"
+										>
+											<div className="user-avatar flex-shrink-0">
+												<img src={UserAvatar} alt="avatar" className="img-fluid" />
+											</div>
+										</a>
+										<ul className="dropdown-menu dropdown-menu-end">
+											<li>
+												<a className="dropdown-item" href="#a">
+													Action
+												</a>
+											</li>
+											<li>
+												<a className="dropdown-item" href="#a">
+													Another action
+												</a>
+											</li>
+											<li>
+												<hr className="dropdown-divider" />
+											</li>
+											<li>
+												<a className="dropdown-item" href="#a">
+													Something else here
+												</a>
+											</li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</nav>
+					<div id="collapseOne" className="mobile-search shadow-sm accordion-collapse collapse">
+						<div className="input-group">
+							<span className="input-group-text pe-0">
+								<i className="far fa-search"></i>
+							</span>
+							<input type="text" className="form-control" placeholder="Search here" />
+							<button className="btn btn-orange z-index-1001" type="button">
+								Search
+							</button>
+							<button className="btn btn-blue z-index-1002" type="button">
+								Expand
+							</button>
+							<button className="btn btn-purple z-index-1003" type="button">
+								Reset
+							</button>
+						</div>
+					</div>
+				</div>
+			</header>
+		</>
+	)
+}
+
+export default ProfileVideoViewHeader
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,5 +1,381 @@
+import ProfileVideoViewHeader from './../components/ProfileVideoViewHeader'
+import UserAvatar from '../assets/images/user-avatar.png'
+import Card1Img from '../assets/images/3d-card/img-1.png'
+import Card2Img from '../assets/images/3d-card/img-2.png'
+import Card3Img from '../assets/images/3d-card/img-3.png'
+
 const Profile = () => {
-	return <div>Profile</div>
+	return (
+		<div className="profile">
+			<ProfileVideoViewHeader />
+
+			{/* Profile Banner */}
+			<div class="profile-banner"></div>
+
+			{/* <!-- === profile-info-full section start === --> */}
+			<section class="profile-info-full mb-5">
+				<div class="container">
+					<div class="card profile-info-card shadow">
+						<div class="profile-info">
+							<div class="user-avatar">
+								<img src={UserAvatar} alt="user-avatar" class="img-fluid shadow-lg" />
+							</div>
+							<h2 class="user-name">Justin Bieber</h2>
+							<p class="user-description">
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit repellat, animi eum, tempora explicabo minima adipisci
+								eos at voluptatibus iure quis fugit deserunt aliquam deleniti illum sapiente ipsa cupiditate sequi!
+							</p>
+						</div>
+
+						<div class="row g-4 py-5 px-lg-4">
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card1Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card2Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card3Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card1Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card2Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card3Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card1Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card2Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card3Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card1Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card2Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card3Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card1Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card2Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card3Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card1Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card2Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+							<div class="col-lg-4 col-sm-6">
+								<a href="#a">
+									<div class="card-video darkMode-elable">
+										<div class="img border-radius-20">
+											<div class="thumbnail position-relative">
+												<img src={Card3Img} alt="card-thumbnail" class="w-100" />
+												<div class="overlay"></div>
+												<span class="video-length">24:08</span>
+											</div>
+										</div>
+										<div class="text d-flex">
+											<div class="flex-grow-1">
+												<h3 class="title">Namver Say Naver (Official Music video)</h3>
+												<p class="creator-name">Justin Bieber ft.Joden Smith</p>
+											</div>
+										</div>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	)
 }
 
 export default Profile
